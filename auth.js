0dde5cbfcea3ae3447c9834eb7a365e8347eafd5
adrian18chacha-bit <adrian.18chacha@gmail.com>
2026-07-02 11:13:32 -0500

    Add files via upload

--- auth.js
+++ auth.js
@@ -53,5 +53,11 @@ async function checkUser() {
             splash.style.opacity = '0';
             setTimeout(() => splash.style.display = 'none', 500);
         }
+    } else {
+        const splash = document.getElementById('splash-screen');
+        if (splash) {
+            splash.style.opacity = '0';
+            setTimeout(() => splash.style.display = 'none', 500);
+        }
     }
 }
